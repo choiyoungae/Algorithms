1311d194dc48ec8fbfae7c77252eb1e2f03246be
Choi Youngae <choiaeyoung1001@gmail.com>
2025-10-08 12:31:11 +0900

    Create Programmers_타겟넘버.js

---
+++ JavaScript/Programmers_타겟넘버.js
@@ -0,0 +1,20 @@
+function solution(numbers, target) {
+    let answer = 0;
+    const length = numbers.length;
+
+    function dfs(index, sum) {
+        if (index === length) {
+            if (target === sum) {
+                answer++;
+            }
+            return;
+        }
+
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+
+    dfs(0, 0);
+
+    return answer;
+}
