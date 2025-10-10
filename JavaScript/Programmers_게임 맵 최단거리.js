8b28b5e8dff266bc5a70f300ad17269e33a75335
choiyoungae <109134495+choiyoungae@users.noreply.github.com>
2025-10-10 23:47:06 +0900

    Update Programmers_게임 맵 최단거리.js

--- JavaScript/Programmers_게임 맵 최단거리.js
+++ JavaScript/Programmers_게임 맵 최단거리.js
@@ -1,3 +1,8 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/1844
+
+게임 맵의 상태 maps가 매개변수로 주어질 때, 캐릭터가 상대 팀 진영에 도착하기 위해서 지나가야 하는 칸의 개수의 최솟값을 return 하도록 solution 함수를 완성해주세요. 단, 상대 팀 진영에 도착할 수 없을 때는 -1을 return
+*/
 function solution(maps) {
     const n = maps[0].length;  // 가로
     const m = maps.length;     // 세로
